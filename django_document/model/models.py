2f5424f54a35c888d115c033bcbbcb4a304b68f5
Kim JinSeok <jskim2code@gmail.com>
2017-10-10 13:48:28 +0900

    admin, car-manufacturer

--- django_document/model/models.py
+++ django_document/model/models.py
@@ -10,4 +10,27 @@ class Person(models.Model):
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
+
 # DB makemigrations, migrate
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'제조사: {self.name}'
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return f'''모델명: {self.name}
+         제조사: {self.manufacturer.name}'''
+
